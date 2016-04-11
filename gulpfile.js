1383e750866c0f219681b692c70afe11a0f4709e
todoapam <todoapam@gmail.com>
2016-04-11 04:11:26 -0300

    Añadido archivos coffeescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
-  gutil = require('gulp-util');
+  gutil = require('gulp-util'),
+  coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-  gutil.log('Workflow are awesome');
+var coffeeSources = ['components/coffee/tagline.coffee']
+gulp.task('coffee', function() {
+  gulp.src('coffeeSources')
+    .pipe(coffee({bare: true})
+    .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
